6537dc075bf397c26320a4b534034c0d81ce398f
NeginAryapour <negin_aryapour@hotmail.com>
2018-02-01 12:23:31 +0330

    Implement ascii to decimal in python

---
+++ 01-ascii_to_decimal/main.py
@@ -0,0 +1,15 @@
+def ascii_to_decimal(string):
+    if len(string) == 0:
+        return 0
+    number = ord(string[0]) - 48
+    power = len(string) - 1
+    return number * (10 ** power) + ascii_to_decimal(string[1:])
+
+
+if __name__ == '__main__':
+    if ascii_to_decimal('12345') == 12345:
+        print(True)
+    else:
+        print(False)
+
+
